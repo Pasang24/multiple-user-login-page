69b86ea56702b5b0f5182daa15fcc545dfc28178
Nissan Sherpa <pemshera@gmail.com>
2023-06-14 21:27:19 +0545

    backend bata img aayo balla ..

--- src/components/job_components/Job.jsx
+++ src/components/job_components/Job.jsx
@@ -2,15 +2,27 @@ import { useNavigate } from "react-router-dom";
 import "./Job.css";
 
 function Job({ job }) {
+  console.log(job.images.length);
+  console.log(job.images);
+  console.log(job);
   const navigate = useNavigate();
   return (
+
     <div className="job-container" onClick={() => navigate(`/jobs/${job._id}`)}>
       <div className="company-image">
-        {/* Added static image for now */}
-        <img
-          alt="logo"
-          src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU"
-        />
+
+        {
+          job.images.length == 0
+            ?
+            <img
+              alt="logo"
+              src="https://play-lh.googleusercontent.com/PCpXdqvUWfCW1mXhH1Y_98yBpgsWxuTSTofy3NGMo9yBTATDyzVkqU580bfSln50bFU"
+            />
+            :
+            <img src={`https://backendapi-znii.onrender.com/${job.images}`} alt="..." />
+
+        }
+
       </div>
       <div className="job-desc">
         <h3>{job.title}</h3>
