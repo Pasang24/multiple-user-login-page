e3d3062e62a4a8a1f33c835ccf613a6d2da50520
Pasang Lama <pasangtheeng14@gmail.com>
2023-06-14 11:38:30 +0545

    Didn't do anything on purpose here

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ import LoginPage from "./pages/LoginPage";
 import NewAccountPage from "./pages/NewAccountPage";
 import NavBar from "./components/custom_components/NavBar";
 
-import ProtectedRoute from './components/ProtectedRoute';
-import AppliedJobs from "./pages/jobs/AppliedJobs"
-import UpsertJobs from "./pages/jobs/UpsertJobs"
-import ViewJob from "./pages/jobs/ViewJob"
+import ProtectedRoute from "./components/ProtectedRoute";
+import AppliedJobs from "./pages/jobs/AppliedJobs";
+import UpsertJobs from "./pages/jobs/UpsertJobs";
+import ViewJob from "./pages/jobs/ViewJob";
 
 function App() {
   const [hasLoggedIn, setHasLoggedIn] = useState(
@@ -27,19 +27,18 @@ function App() {
             />
             <Route path="/signup" element={<NewAccountPage />} />
 
-            <Route path='jobs'>
-              <Route path=':id' element={<ViewJob />} />
+            <Route path="jobs">
+              <Route path=":id" element={<ViewJob />} />
 
               <Route element={<ProtectedRoute role="applicant" />}>
-                <Route path='applied' element={<AppliedJobs />} />
+                <Route path="applied" element={<AppliedJobs />} />
               </Route>
 
               <Route element={<ProtectedRoute role="recruiter" />}>
-                <Route path='create' element={<UpsertJobs />} />
-                <Route path='edit/:id' element={<UpsertJobs />} />
+                <Route path="create" element={<UpsertJobs />} />
+                <Route path="edit/:id" element={<UpsertJobs />} />
               </Route>
             </Route>
-
           </>
         )}
         <Route path="*" element={<h1>404 Page Not Found</h1>} />
