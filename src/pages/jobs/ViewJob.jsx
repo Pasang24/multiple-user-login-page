d559ee61ea48f79707b62c6b2e62b8d0cf457196
Pasang Lama <pasangtheeng14@gmail.com>
2023-06-14 16:26:39 +0545

    Showing the ViewJob Component on clicking individual job

--- src/pages/jobs/ViewJob.jsx
+++ src/pages/jobs/ViewJob.jsx
@@ -1,10 +1,9 @@
 function ViewJob() {
-    return (
-      <div>
-        <h1 style={{ color: "white", textAlign: "center" }}>ViewJob</h1>
-      </div>
-    );
-  }
-  
-  export default ViewJob;
-  
+  return (
+    <div>
+      <h1 style={{ color: "white", textAlign: "center" }}>ViewJob</h1>
+    </div>
+  );
+}
+
+export default ViewJob;
